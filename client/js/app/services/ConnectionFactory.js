8c2c23fb7140b71039805a0a256798c9f9ee1a4b
Romulo Augusto <romulomail@gmail.com>
2020-02-29 17:06:26 -0300

    cria a classe ConnectionFactory para encapsular a criação das conexões ao banco

---
+++ client/js/app/services/ConnectionFactory.js
@@ -0,0 +1,61 @@
+const ConnectionFactory = (function() {
+    const dbName = "aluraframe";
+    const dbVersion = 4;
+    const stores = ["megociacoes"];
+
+    var connection;
+    var closeFunction;
+
+    return class ConnectionFactory {
+
+        constructor() {
+            throw new Error("Não é possível criar instância de ConnectionFactory");
+        }
+
+        static getConnection() {
+
+            return new Promise((resolve, reject) => {
+
+                let openRequest = window.indexedDB.open(dbName, dbVersion);
+                openRequest.onupgradeneeded = e => {
+                    ConnectionFactory._createStores(e.target.result);
+                };
+
+                openRequest.onsuccess = e => {
+                    if (!connection) {
+                        connection = e.target.result;
+                        closeFunction = connection.close.bind(connection);
+                        connection.close = () => {
+                            throw new Error("A conexão não pode ser fechada diretamente");
+                        }
+                    }
+                    resolve(connection);
+                };
+
+                openRequest.onerror = e => {
+                    console.log(e.target.error);
+                    reject(e.target.error.name);
+                };
+            });
+        }
+
+        static _createStores(connection) {
+            stores.forEach(s => {
+                    
+                if (connection.objectStoreNames.contains(s)) {
+                    connection.deleteObjectStore(s);
+                }
+                connection.createObjectStore(s, {
+                    autoIncrement: true
+                });
+            });
+        }
+
+        static closeConnection() {
+            if (connection) {
+                closeFunction();
+                connection = null;
+            }
+        }
+    }
+})();
