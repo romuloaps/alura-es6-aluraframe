608e33426394752b19868da957cb06735580f9eb
Romulo Augusto <romulomail@gmail.com>
2020-03-02 06:55:45 -0300

    implementação do httpsetvice usando fetch api

--- client/js/app/services/HttpService.js
+++ client/js/app/services/HttpService.js
@@ -1,25 +1,22 @@
 class HttpService {
 
     get(url) {
-        return new Promise((resolve, reject) => {
+        return fetch(url)
+                .then(res => this._handleErrors(res))
+                .then(res => res.json())
+    }
 
-            let xhr = new XMLHttpRequest();
-    
-            xhr.open("GET", url);
-            xhr.onreadystatechange = () => {
+    post(url, dado) {
+        return fetch(url, {
+            headers: {"Content-type": "application/json"},
+            method: "post",
+            body: JSON.stringify(dado)
+        })
+        .then(res => this._handleErrors(res));
+    }
 
-                if (xhr.readyState == 4) {
-
-                    if (xhr.status == 200) {
-                        resolve(JSON.parse(xhr.responseText));
-    
-                    } else {
-                        reject(xhr.responseText);
-                    }
-                }
-            };
-    
-            xhr.send();
-        });
+    _handleErrors(response) {
+        if (!response.ok) throw new Error(response.statusText);
+        return response;
     }
 }
