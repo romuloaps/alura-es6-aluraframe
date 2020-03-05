7d07aeb6b5445a2b44b022c935bdfdeb71a67c3f
Romulo Augusto <romulomail@gmail.com>
2020-03-05 11:13:22 -0300

    configuração do babel 7+ com systemjs e core-js@3 (WIP)

--- client/js/index.js
+++ client/js/index.js
@@ -1,6 +1,8 @@
-import {NegociacaoController} from "./app/controllers/NegociacaoController";
+// import {NegociacaoController} from "./app/controllers/NegociacaoController";
+import {Negociacao, Dates} from "./app/helpers/Dates";
+console.log(Dates.parse("10/10/2020"));
 
-let  negociacaoController = new NegociacaoController();
+// let  negociacaoController = new NegociacaoController();
 
-document.querySelector(".form").addEventListener("onsubmit", negociacaoController.adiciona.bind(negociacaoController));
-document.querySelector("#apagar").addEventListener("onclick", negociacaoController.apaga.bind(negociacaoController));
+// document.querySelector(".form").addEventListener("onsubmit", negociacaoController.adiciona.bind(negociacaoController));
+// document.querySelector("#apagar").addEventListener("onclick", negociacaoController.apaga.bind(negociacaoController));
